ff03b677fad8f964424fc85013b177fcd428d55b
twongkee <twongkee@gmail.com>
2020-03-09 07:10:15 -0400

    after new build, add these

---
+++ root/redo.sh
@@ -0,0 +1,13 @@
+
+add-apt-repository ppa:jonathonf/ffmpeg-4
+apt update
+apt install git
+apt install tmux
+apt install minicom
+apt install htop
+apt update
+apt install ffmpeg
+apt install v4l-utils
+apt install imagemagick
+echo "if timezone still not working on ui"
+echo dpkg-reconfigure tzdata
