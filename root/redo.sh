76e62d037b718748ea4529205697af02a0fff04c
twongkee <twongkee@gmail.com>
2020-04-04 19:25:35 -0400

    cleaner pics

--- root/redo.sh
+++ root/redo.sh
@@ -1,13 +1,13 @@
 
 add-apt-repository ppa:jonathonf/ffmpeg-4
 apt update
+apt install ffmpeg
 apt install git
 apt install tmux
 apt install minicom
 apt install htop
-apt update
-apt install ffmpeg
 apt install v4l-utils
 apt install imagemagick
+apt install bc
 echo "if timezone still not working on ui"
 echo dpkg-reconfigure tzdata
